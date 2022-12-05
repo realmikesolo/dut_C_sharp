82c5789dc975b6d42f56297d45b8ef8db1cd8241
realmikesolo <aeryoff@gmail.com>
2022-12-05 21:45:28 +0100

    lab3

---
+++ lab3_dut/lab3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Random rnd = new Random();
+
+            Console.WriteLine("Введіть 1 цифру: ");
+            string a = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Введіть 2 цифру: ");
+            string b = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Введіть 3 цифру: ");
+            string c = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Введіть 4 цифру: ");
+            string d = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Отримане число: ");
+
+            if (a == c && b == d)
+            {
+                Console.WriteLine("Сума двох перших цифр даного чотиризначного числа рівна сумі двох його останніх цифр");
+            }
+            else
+            {
+                Console.WriteLine("Сума двох перших цифр даного чотиризначного числа не рівна сумі двох його останніх цифр");
+            }
+
+        }
+    }
+}
