51e2741ad25acf8d1b8a8f30a0a57001d48a3a96
realmikesolo <aeryoff@gmail.com>
2022-12-21 16:21:44 +0100

    lab4

---
+++ lab4_dut/laba4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace laba4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = new int[] { 2, 9, 4, 3, 5, 1, 7 };
+            int temp = 0;
+
+            for (int i = 0; i <= intArray.Length - 1; i++)
+            {
+                for (int j = i + 1; j < intArray.Length; j++)
+                {
+                    if (intArray[i] > intArray[j])
+                    {
+                        temp = intArray[i];
+                        intArray[i] = intArray[j];
+                        intArray[j] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Впорядкований масив за зростанням");
+
+            Console.WriteLine("[{0}]", string.Join(", ", intArray));
+
+            for (int i = 0; i <= intArray.Length - 1; i++)
+            {
+                for (int j = i + 1; j < intArray.Length; j++)
+                {
+                    if (intArray[i] < intArray[j])
+                    {
+                        temp = intArray[i];
+                        intArray[i] = intArray[j];
+                        intArray[j] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Впорядкований масив за спаданням");
+            Console.WriteLine("[{0}]", string.Join(", ", intArray));
+
+        }
+    }
+}
